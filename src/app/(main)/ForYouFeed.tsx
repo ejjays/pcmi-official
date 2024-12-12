3ba4f158f55c03a80b42b4d8add976d68abc6c31
ejjays <christsonalloso021@gmail.com>
2024-12-13 04:44:25 +0800

    Update ForYouFeed.tsx

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -53,7 +53,7 @@ export default function ForYouFeed() {
 
   return (
     <InfiniteScrollContainer
-      className="space-y-5"
+      className="space-y-3"
       onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
     >
       {posts.map((post) => (
